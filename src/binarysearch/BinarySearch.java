b688e1c4ce398d59d646f565f360d18fc10eb6a0
Gopi Gorantala <gopishiva001@outlook.com>
2022-04-26 08:45:44 +0530

    time space complexities added

--- src/binarysearch/BinarySearch.java
+++ src/binarysearch/BinarySearch.java
@@ -5,12 +5,13 @@ public class BinarySearch {
     System.out.println(binarySearch(new int[] {-1, 0, 3, 5, 9, 12}, 9));
   }
 
+  //  Time: O(logn),  Space: O(1), where n = length of numbers array
   private static int binarySearch(int[] nums, int target) {
     int low = 0;
     int high = nums.length - 1;
 
     while (low <= high) {
-      int middle = low + (high - low) / 2;
+      int middle = low + (high - low) / 2; // for overflows.
 
       int number = nums[middle];
 
--- src/maths/IsPrime.java
+++ src/maths/IsPrime.java
@@ -1,3 +1,5 @@
+package maths;
+
 public class IsPrime {
   public static void main(String[] args) {
     System.out.println(isPrime(2));
