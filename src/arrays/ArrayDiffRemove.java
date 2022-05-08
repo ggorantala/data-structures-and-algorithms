d80c23ff3b4e7c0bf9c143c67819a0e665017af0
Gopi Gorantala <gopishiva001@outlook.com>
2022-05-08 21:34:31 +0530

    some more problems

---
+++ src/arrays/ArrayDiffRemove.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ArrayDiffRemove {
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(arrayDiff(new int[] {1, 2}, new int[] {1})));
+    System.out.println(Arrays.toString(arrayDiff(new int[] {1, 2, 2, 2, 3}, new int[] {2})));
+  }
+
+  public static int[] arrayDiff(int[] a, int[] b) {
+    // Your code here
+    return a;
+  }
+}
